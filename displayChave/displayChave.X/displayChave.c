4dabdda29a1c848ade3c30e9be4b9ecd85b4c8f0
Reverdan Almeida Sparinger <reverdan@gmail.com>
2021-10-19 19:08:45 -0300

    display e chave

---
+++ displayChave/displayChave.X/displayChave.c
@@ -0,0 +1,48 @@
+
+#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator: High-speed crystal/resonator on RA6/OSC2/CLKOUT and RA7/OSC1/CLKIN)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = ON       // RA5/MCLR/VPP Pin Function Select bit (RA5/MCLR/VPP pin function is MCLR)
+#pragma config BOREN = OFF      // Brown-out Detect Enable bit (BOD disabled)
+#pragma config LVP = OFF        // Low-Voltage Programming Enable bit (RB4/PGM pin has digital I/O function, HV on MCLR must be used for programming)
+#pragma config CPD = OFF        // Data EE Memory Code Protection bit (Data memory code protection off)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
+#include <xc.h>
+
+#define _XTAL_FREQ 4000000
+
+#define chaveMenos !PORTAbits.RA1
+#define chaveMais !PORTAbits.RA2
+
+int display[10] = {0xee, 0x28, 0xcd, 0x6d, 0x2b, 0x67, 0xe7, 0x2c, 0xef, 0x6f};
+
+void configuracao()
+{
+    TRISB = 0x00;
+    PORTB = 0x00;
+}
+
+void main()
+{
+    configuracao();
+    unsigned int valor = 0;
+    for (;;)
+    {
+        if (chaveMais)
+        {
+            while (chaveMais) __delay_ms(300);
+            valor++;
+            if (valor > 9)
+                valor = 0;
+        }
+        if (chaveMenos)
+        {
+            while(chaveMenos) __delay_ms(300);
+            valor--;
+            if (valor == 65535)
+                valor = 9;
+        }
+        PORTB = display[valor];
+    }
+}
